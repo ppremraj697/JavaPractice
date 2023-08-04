9bdf1f7c84630ad93b9ad3e6734e1f8f369e5a24
Premraj Patil <ppremraj697@gmail.com>
2023-08-04 12:46:23 +0530

    Everything about Anonymous Inner Class

--- JavaPractice/src/innerClass/Test2.java
+++ JavaPractice/src/innerClass/Test2.java
@@ -1,13 +1,29 @@
 package innerClass;
 
+interface AnoInterface{
+	
+	void iDisplay();	
+}
+
 class Outer2{
 	
 	int x = 10;
 	static int y = 20;
 	
+	//THIS CAN BE INSIDE METHOD SCOPE ALSO
+	//IF INSIDE METHOD THEN METHOD-LOCAL VARIABLE MUST BE EFFECTIVELY FINAL FOR ANONYMOUS INNER CLASS
+	//IF DIRECTLY IN CLASS THEN CAN UPDATE CLASS VARIABLE 
+	AnoInterface ai = new AnoInterface() {
+		
+		public void iDisplay() {
+			System.out.println("Implementing interface as Anonymous Inner Class");
+		}
+	};
+	
 	void nonStaticOuterDisplay() {
 		System.out.println("nonStaticOuterDisplay() Method");
-		System.out.println(x+" "+y);
+		System.out.println(x+" "+y);	
+		ai.iDisplay();
 	}
 	
 	static void staticOuterDisplay() {
@@ -21,5 +37,14 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		Outer2 o = new Outer2();
+		o.nonStaticOuterDisplay();
+		
+		AnoInterface ai2 = new AnoInterface() {
+			public void iDisplay() {
+				System.out.println("Implementing interface inside main method");
+			}
+		};
+		
+		ai2.iDisplay();
 	}
 }
